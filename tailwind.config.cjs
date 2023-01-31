2c518cad558a10d7fe06efa245357a86a56d5346
mateus-cristian <mateus_cristian@outlook.com>
2023-01-30 21:38:54 -0300

    feat:colors and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        text: ["Heebo", "sans-serif"],
+        title: ["Poppins", "sans-serif"],
+        especial: ["Pacifico", "cursive"],
+      },
+      colors: {
+        "blue-500": "#4DBCE9",
+        "blue-700": "#26ADE4",
+        "grey-500": "#777777",
+        "grey-700": "#444444",
+        pantone: "#F1F1F1",
+        black: "#222222",
+      },
+    },
   },
   plugins: [],
 };
