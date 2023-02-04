47c1bc83102fc1bfb94c21616cc56bf1ab7852b3
mateus-cristian <mateus_cristian@outlook.com>
2023-02-04 10:39:54 -0300

    feat: adding java and spring to project

---
+++ src/java/src/main/java/com/socialblog/socialblog/SocialblogApplication.java
@@ -0,0 +1,13 @@
+package com.socialblog.socialblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialblogApplication.class, args);
+	}
+
+}
