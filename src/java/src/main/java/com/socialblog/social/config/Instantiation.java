b6fb709d1bba9057537ac9dfe60a647cf5be0885
mateus-cristian <mateus_cristian@outlook.com>
2023-02-07 19:49:46 -0300

    feat: seed database

---
+++ src/java/src/main/java/com/socialblog/social/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.socialblog.social.config;
+
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.socialblog.social.entities.User;
+import com.socialblog.social.repositories.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        
+        userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+    }
+    
+}
