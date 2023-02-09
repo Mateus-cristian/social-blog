74fcbf63f8659fc94e8e7e0368de62117edc20c7
mateus-cristian <mateus_cristian@outlook.com>
2023-02-09 18:41:24 -0300

    feat: create user method

--- src/java/src/main/java/com/socialblog/social/resources/UserResource.java
+++ src/java/src/main/java/com/socialblog/social/resources/UserResource.java
@@ -1,38 +1,52 @@
 package com.socialblog.social.resources;
 
-
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
-
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.socialblog.social.dto.UserDTO;
 import com.socialblog.social.entities.User;
 import com.socialblog.social.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 
     @Autowired
     UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = userService.findAll();
-        
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
-    
-    @GetMapping(value="/{id}")
+
+    @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable String id) {
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
     }
-    
+
+    @PostMapping
+    public ResponseEntity<Void> insertUser(@RequestBody UserDTO userDto) {
+        User obj = userService.fromDTO(userDto);
+        obj = userService.insertUser(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
 }
--- src/java/src/main/java/com/socialblog/social/services/UserService.java
+++ src/java/src/main/java/com/socialblog/social/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.socialblog.social.dto.UserDTO;
 import com.socialblog.social.entities.User;
 import com.socialblog.social.repositories.UserRepository;
 import com.socialblog.social.services.exception.ObjectNotFoundException;
@@ -16,18 +17,24 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
-    public List<User> findAll(){
+    public List<User> findAll() {
         return userRepository.findAll();
     }
 
-    public User findById(String id){
-        try{
+    public User findById(String id) {
+        try {
             Optional<User> user = userRepository.findById(id);
-
             return user.get();
-        }catch(RuntimeException e){
+        } catch (RuntimeException e) {
             throw new ObjectNotFoundException("Objeto não encontrado");
         }
     }
-    
+
+    public User insertUser(User obj) {
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
 }
